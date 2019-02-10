9539a267ac38e927e179decc1263baeab2b40761
tamilnathan <tamilnathank@gmail.com>
2019-02-10 18:11:26 +0530

    Structural Changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,52 +12,9 @@ export class HomeComponent implements OnInit{
     constructor(private router:Router){}
 
     ngOnInit(){
-        this.setClients();
-        this.setParallaxEffectForContent();
+       
     }
 
-
-    private setClients():void{
-        $(".owl-carousel").owlCarousel({
-            items: 5,
-            nav:true,
-            autoplay:true,
-            autoplayTimeout:5000,
-            autoplayHoverPause:true,
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:3
-                },
-                1000:{
-                    items:5
-                }
-            }
-        });
-        $( ".owl-prev").html('<img src="assets/images/svg/arrow-left.svg" width="32" />');
-        $( ".owl-next").html('<img src="assets/images/svg/arrow-right.svg" width="32" />');
-    }
-
-
-    private setParallaxEffectForContent():void{
-       //Parallax Effect
-        $(window).scroll(function(){
-            $('.parallax').each(function(){
-                if ($(this).offset().top < $(window).scrollTop()) {
-                var difference = $(window).scrollTop() - $(this).offset().top;
-                var half = (difference / 2) + 'px',
-                transform = 'translate3d( 0, ' + half + ',0)';
-                $(this).find('img').css('transform', transform);
-            } else {
-                $(this).find('img').css('transform', 'translate3d(0,0,0)');
-            }
-            });
-
-        });
-    }
-
-
+   
    
 }
---
+++ src/assets/js/app.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 5,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    $( ".owl-prev").html('<img src="assets/images/svg/arrow-left.svg" width="32" />');
+    $( ".owl-next").html('<img src="assets/images/svg/arrow-right.svg" width="32" />');
+});
+
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      mobMenu.hideMenu();
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+});
+
+//Parallax Effect
+$(window).scroll(function(){
+    $('.parallax').each(function(){
+        if ($(this).offset().top < $(window).scrollTop()) {
+        var difference = $(window).scrollTop() - $(this).offset().top;
+        var half = (difference / 2) + 'px',
+        transform = 'translate3d( 0, ' + half + ',0)';
+        $(this).find('img').css('transform', transform);
+    } else {
+        $(this).find('img').css('transform', 'translate3d(0,0,0)');
+    }
+    });
+
+});
+
+
+//Form Validation
+var validateForm = (function(){
+    var email, mobile, flag;
+    email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    mobile = /^\d+$/;
+    flag = 0;
+    return {
+        emptyValid: function(ths){
+            if(ths.value == ""){
+                $(ths).addClass("is-invalid");
+                flag = 0;   
+            }else if(ths.value.length < ths.minLength){
+                $(ths).addClass("is-invalid");
+                flag = 0;   
+            }else{
+                $(ths).removeClass("is-invalid");
+                flag = 1;
+            }
+        },
+        emailValid: function(ths){
+            if( !email.test(ths.value) ){
+                $(ths).addClass("is-invalid");
+                flag = 0;
+            }else{
+                $(ths).removeClass("is-invalid");
+                flag = 1;
+            }
+        },
+        mobileValid: function(ths){
+            if( !mobile.test(ths.value) ){
+                $(ths).addClass("is-invalid");
+                flag = 0;
+            }else if(ths.value.length < ths.minLength){
+                $(ths).addClass("is-invalid");
+                flag = 0;
+            }else{
+                $(ths).removeClass("is-invalid");
+                flag = 1;
+            }
+        },
+        formValid: function(ths){
+            if(flag == 0){
+                $(ths).find(".alert").removeClass('hide').addClass('show');
+                setTimeout(function(){ $(ths).find(".alert").removeClass('show').addClass('hide'); }, 3000);
+                return false;
+            }else{
+                $(ths).find(".alert").removeClass('show').addClass('hide');
+                return true;
+            }
+        }
+    }
+})();
+
+//Hamburger Menu for Mobile
+var mobMenu = (function(){
+    var navSelector = $(".nav");
+    return {
+        showMenu: function(){
+            navSelector.animate({right: '0'});
+        },
+        hideMenu: function(){
+            navSelector.animate({right: '-220px'});
+        }
+    };
+})();
+$(".hamburger").on('click',mobMenu.showMenu);
+$(".mobileClose").on('click',mobMenu.hideMenu);
