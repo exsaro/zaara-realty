0678c921fbc7b662f4bbc78341640890e5c2e9ba
exsaro <saravanan.rsn@gmail.com>
2019-03-15 18:25:43 +0530

    Added view page design

--- src/app/pages/project-details/project-details.component.ts
+++ src/app/pages/project-details/project-details.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector:'app-view',
-    templateUrl:'project-details.component.html'
+    templateUrl:'project-details.component.html',
+    styleUrls: ['./project-details.component.css']
 })
 
 export class ProjectDetailsComponent implements OnInit{
