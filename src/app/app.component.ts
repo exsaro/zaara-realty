c1d1bd59d67a4bc249820f5b0274a7aceb1c2517
hari42726 <hari@sis.in>
2020-01-20 18:06:37 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  private recaptchaSiteKey = '6Lfki38UAAAAADienDrSAirDZ7LdEWmU3SnDtTdc';
   title = 'zaara-homes';
 
 
